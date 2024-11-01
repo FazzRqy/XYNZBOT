a042950c73a5125acd57546f66677395d8993e3a
FazzRqy <ajafarel095@gmail.com>
2024-11-01 21:22:24 +0700

    XYNZBOT

--- command/ai/openai.js
+++ command/ai/openai.js
@@ -1,25 +1,30 @@
 export default {
     command: ["openai", "chatgpt", "ai"],
-    description: "Search some informasion using gpt4",
+    description: "Search some informasion using chatgpt (GPT-4 Mini)",
     name: "openai",
     tags: "ai",
 
     run: async (m, { text }) => {
-        if (!text) m.reply(`try chat with ai/openai. \n\nexample: ${m.prefix + m.command} apa nama rumus kimia dari besi`)
-        try {
-            const data = API("nyxs","/ai/gpt4", { text: text })
-            let response
+        if (!text) return m.reply(`example use: ${m.prefix + m.command} list of snipers`);
 
-            console.log(data)
+            const data = API("neoxr","/ai/gpt4-mini", { text: text }, { apikey: globalThis.APIKeys.neoxr});
+            let response;
+
+            console.log(data);
     
-            response = await func.fetchJson(data)
+            try {
+                response = await func.fetchJson(data);
+            } catch (error) {
+                console.error(error);
+            };
     
-            console.log(response)
+            console.log(response);
             
-            await m.reply(response.result)
-        } catch (error) {
-            console.error(error);
-            m.reply('Sorry, there seems to be an error: ' + error)
-        }
-    }
-}
+            try {
+                await m.reply(response.result);
+            } catch (error) {
+                console.error(error);
+                m.reply("Error");
+            };
+    },
+};
--- command/download/facebook.js
+++ command/download/facebook.js
@@ -6,85 +6,74 @@ export default {
 
     run: async (m, { conn }) => {
         
-        const Url = m.args[0]
-        const apikeys = global.APIKeys.neoxr
+        const Url = m.args[0];
+        const apikeys = global.APIKeys.neoxr;
 
-        if (!Url) {
-            m.reply(`Invalid URL\n\nContoh: ${m.prefix + m.command} https://www.facebook.com/reel/986009916077834?mibextid=xCPwDs&s=yWDuG2&fs=e`,);
-        } else {
+        m.reply(global.msg.dlloading);
 
-            let cekUrl = /https:\/\/(www\.)?facebook\.com\/.*/
+        if (!Url) return setTimeout(() => { m.reply(`\`\`\`Example: ${m.prefix + m.command} https://www.facebook.com/reel/986009916077834?mibextid=xCPwDs&s=yWDuG2&fs=e\`\`\``); }, 1000);
 
-            let match = Url.match(cekUrl)
+            const cekUrl = /https:\/\/(www\.)?facebook\.com\/.*/;
 
-            if (!match) {
-                m.reply("Make sure you put a Facebook URL, not the other URL!")
-            } else {
+            const match = Url.match(cekUrl);
 
-        const ApiUrl = `https://api.neoxr.eu/api/fb?url=${encodeURIComponent(Url)}&apikey=${apikeys}`
+            if (!match) return m.reply("Make sure you put a Facebook URL, not the other URL!");
+
+        const ApiUrl = `${global.APIs.neoxr + "/api/fb?url=" + encodeURIComponent(Url) + "&apikey=" + apikeys}`;
 
         console.log(ApiUrl);
         let response;
 
         try {
-            response = await func.fetchJson(ApiUrl)
+            response = await func.fetchJson(ApiUrl);
         } catch (error) {
-            console.error(error)
-        }
+            console.error(error);
+        };
 
-        console.log(response)
+        console.log(response);
 
         if (m.args[1] === '--hd') {
-        let url = response.data[1].url
+            let url = response.data[1].url;
 
-        console.log(url)
+            if (url == false) return m.reply("error, Make sure you put a correct url/link!");
+            let sendFile = url;
 
-        if (url == false) {
-            m.reply("error, Make sure you put a correct url/link!")
-        } else {
-        let sendFile = url
-
-        try {
-            await m.reply( sendFile, { caption: "Done" })
-        } catch (err) {
-            m.reply("There was a slight problem while sending the video.")
-            console.error(err);
+            try {
+                await m.reply( sendFile, { caption: "Done" });
+            } catch (err) {
+                m.reply("There was a slight problem while sending the video.");
+                console.error(err);
+            };
         }
-    }
-    } else if (m.args[1] === '--sd') {
-        let url = response.data[0].url
+        if (m.args[1] === '--sd') {
+            let url = response.data[0].url;
 
-        console.log(url)
+            console.log(url);
 
-        if (url == false) {
-            m.reply("error, Make sure you put a correct url/link!")
-        } else {
-        let sendFile = url
+            if (url == false) return m.reply("error, Make sure you put a correct url/link!");
+            let sendFile = url;
 
-        try {
-            await m.reply( sendFile, { caption: "Done" })
-        } catch (err) {
-            m.reply("There was a slight problem while sending the video.")
-            console.error(err);
-        }
-    }
+            try {
+                await m.reply( sendFile, { caption: "Done" });
+            } catch (err) {
+                m.reply("There was a slight problem while sending the video.");
+                console.error(err);
+            };
     } else {
             let url = response.data[0].url
 
             if (url)
 
-            console.log(url)
+            console.log(url);
     
-            let sendFile = url
+            let sendFile = url;
     
             try {
-                await m.reply( sendFile, { caption: "Done\n\n*Note*:\n`If you want to change the resolution of this video, try adding '--hd/sd' after you paste the link you copied, if you don't add it then it will be download the video in SD quality (following the default)`" }, wm)
+                await m.reply( sendFile, { caption: "Done\n\n*Note*:\n`If you want to change the resolution of this video, try adding '--hd/sd' after you paste the link you copied, if you don't add it then it will be download the video in SD quality (following the default)`" }, wm);
             } catch (err) {
-                m.reply("There was a slight problem while sending the video.")
+                m.reply("There was a slight problem while sending the video.");
                 console.error(err);
-            }
-    }
-    }
-    }
-}
-}
+            };
+    };
+},
+};
